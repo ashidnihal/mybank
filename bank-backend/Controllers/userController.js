d26feaaa3ba5aff49e29b641384ef1c5a0035d72
ashidnihal <118710480+ashidnihal@users.noreply.github.com>
2024-11-10 10:52:49 +0530

    Updated project with new features

--- bank-backend/Controllers/userController.js
+++ bank-backend/Controllers/userController.js
@@ -14,7 +14,7 @@ exports.register = async (req, res) => {
         const newUser = new users({
             username,
             email,
-            password: hashedPassword, // Store the hashed password
+            password: hashedPassword, 
             accountNumber,
             isAdmin,
             balance: "",
@@ -35,7 +35,7 @@ exports.login = async (req, res) => {
 
     try {
         const existingUser = await users.findOne({ email });
-        if (existingUser && await bcrypt.compare(password, existingUser.password)) { // Compare hashed passwords
+        if (existingUser && await bcrypt.compare(password, existingUser.password)) { 
             const token = jwt.sign(
                 { userid: existingUser._id },
                 "super2024",
@@ -49,131 +49,7 @@ exports.login = async (req, res) => {
         res.status(500).json("Login failed: " + err);
     }
 };
-// exports.login =async(req,res)=>{
-//     // accep data from client
-//     const {email,password}=req.body
-//     try{
-//         const existingUser=await users.findOne({email,password})
-//         if(existingUser){
-//             const token= jwt.sign({userid:existingUser._id},"super2024")
-//             console.log(token);
-//             res.status(200).json({existingUser,token})
-//         }else{
-//             res.status(404).json("Invalid email or password")
-//         }
 
-//     }
-//     catch(err){
-//         res.status(500).json("login failed"+err)
-//     }
-// }
-
-// exports.register = async (req, res) => {
-//     console.log("inside register method");
-//     const { username, email, password, accountNumber, isAdmin } = req.body; // Ensure isAdmin is destructured
-//     console.log(username, email, password, accountNumber, isAdmin); // Log isAdmin to check the value
-
-//     try {
-//         // Check if the email is already registered
-//         const existingUser = await users.findOne({ email });
-//         console.log(existingUser);
-//         if (existingUser) {
-//             return res.status(406).json('already registered');
-//         } else {
-//             const newUser = new users({
-//                 username,
-//                 email,
-//                 password,
-//                 accountNumber,
-//                 isAdmin,  // Include isAdmin here
-//                 balance: "",
-//                 address: "",
-//                 phoneNumber: ""
-//             });
-//             await newUser.save();
-//             res.status(200).json(newUser);
-//         }
-//     } catch (err) {
-//         res.status(500).json('Register Failed: ' + err.message);
-//     }
-// };
-
-
-
-// exports.register=async(req,res)=>{
-//     console.log("inside register method");
-//     const {username,email,password,accountNumber}= req.body
-//     console.log(username,email,password,accountNumber);
-//     // accept data from client
-//     try{
-
-//         // check if the email is already registerd
-//         const existingUser = await users.findOne({email})
-//         console.log(existingUser);
-//         if(existingUser){
-//             res.status(406).json('already registered')
-//         }else{
-//             const newUser = new users({
-//                 username,
-//                 email,
-//                 password,
-//                 accountNumber,
-//                 balance:"",
-//                 address:"",
-//                 phoneNumber:""
-
-//             })
-//             await newUser.save()
-//             res.status(200).json(newUser)
-//         }
-       
-//     }
-//     catch(err){
-//         res.status(500).json('Register Failed...')
-//     }
-// }
-
-
-// login logic
-
-
-// exports.login =async(req,res)=>{
-//     // accep data from client
-//     const {email,password}=req.body
-//     try{
-//         const existingUser=await users.findOne({email,password})
-//         if(existingUser){
-//             const token= jwt.sign({userid:existingUser._id},"super2024")
-//             console.log(token);
-//             res.status(200).json({existingUser,token})
-//         }else{
-//             res.status(404).json("Invalid email or password")
-//         }
-
-//     }
-//     catch(err){
-//         res.status(500).json("login failed"+err)
-//     }
-// }
-
-// exports.login = async (req, res) => {
-//     const { email, password } = req.body;
-//     try {
-//         const existingUser = await users.findOne({ email, password });
-//         if (existingUser) {
-//             const token = jwt.sign(
-//                 { userid: existingUser._id, isAdmin: existingUser.isAdmin },  // Include isAdmin in the token
-//                 "super2024",
-//                 { expiresIn: '1h' }
-//             );
-//             res.status(200).json({ existingUser, token });
-//         } else {
-//             res.status(404).json("Invalid email or password");
-//         }
-//     } catch (err) {
-//         res.status(500).json("Login failed: " + err);
-//     }
-// };
 exports.getUserDetails = async (req, res) => {
     const userId=req.payload
     try {
--- bank-frontend/src/App.jsx
+++ bank-frontend/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
      <Route path={'/admin/user'} element={<AdminUser/> } />
      <Route path={'/admin/transactions'} element={<AdminTransaction/> } />
      <Route path={'/admin/transactions'} element={<AdminUser/> } />
-<Route path="*" element={<NotFound />} />
+     <Route path="*" element={<NotFound />} />
 
 
 
@@ -42,38 +42,5 @@ function App() {
   )
 }
 
-// export default App
-// import { Route, Routes } from 'react-router-dom';
-// import './App.css';
-// import Header from './Components/Header';
-// import Login from './pages/Login';
-// import MainDashboard from './pages/MainDashboard';
-// import AdminPanel from './pages/AdminPanel';
-// import Myaccount from './pages/Myaccount';
-// import Transaction from './pages/Transaction';
-// import Payment from './pages/Payment';
-// import AdminUser from './pages/AdminUser';
-// import AdminTransaction from './pages/AdminTransaction';
-// import NotFound from './pages/Notfound'; // Import your NotFound component
-
-// function App() {
-//   return (
-//     <>
-//       <Header />
-//       <Routes>
-//         <Route path={'/'} element={<Login />} />
-//         <Route path={'/register'} element={<Login register />} />
-//         <Route path={'/dashboard'} element={<MainDashboard />} />
-//         <Route path={'/admin'} element={<AdminPanel />} />
-//         <Route path={'/user/myaccount'} element={<Myaccount />} />
-//         <Route path={'/user/transaction'} element={<Transaction />} />
-//         <Route path={'/user/payments'} element={<Payment />} />
-//         <Route path={'/admin/user'} element={<AdminUser />} />
-//         <Route path={'/admin/transactions'} element={<AdminTransaction />} />
-//         <Route path="*" element={<NotFound />} />
-//       </Routes>
-//     </>
-//   );
-// }
 
 export default App;
